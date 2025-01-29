dd8f26fe3c24616461930ded23447dc64ad50294
Nasim Kabir <mdkabir334@gmail.com>
2025-01-29 17:54:32 +0600

    add hello controller

---
+++ src/main/java/com/kabirit/spring_boot_practice/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.kabirit.spring_boot_practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author nasimkabir
+ * @date 29/1/25
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
